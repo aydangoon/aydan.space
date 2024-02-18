c3801fb18b1f244705c682cc87dc8e256bfccf12
Aydan Gooneratne <aydan@disczmusic.com>
2024-02-18 16:28:08 -0500

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="min-h-screen bg-[#e7e8e9]">
+    <div className="min-h-screen bg-[#f7f8f9]">
       <div className="flex flex-col space-y-8">
         <h2 className="text-5xl">Huge Statement</h2>
         <h2 className="text-4xl">Section Header</h2>
@@ -13,7 +13,7 @@ const Home = () => {
             dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
           </p>
         </div>
-        <div className="flex w-[300px] flex-col space-y-2">
+        <div className="flex w-[500px] flex-col space-y-2">
           <h3 className="text-xl">Paragraph Header</h3>
           <p className="text-md">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
@@ -22,6 +22,9 @@ const Home = () => {
             dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
           </p>
         </div>
+      </div>
+      <div className="inline-flex rounded-md bg-gradient-to-r from-blue-400 to-violet-400 px-6 py-2 text-white">
+        button
       </div>
     </div>
   )
