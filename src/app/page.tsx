c3155cf25ec8c17b64479111609eb1778273d523
Aydan Gooneratne <aydan@disczmusic.com>
2024-02-18 14:51:42 -0500

    good skeuomorphism

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
 const Home = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center gap-8 bg-[#f8f8fe]">
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-8 bg-[#f8f8fe]">
+      <div className="font-mono text-6xl">Hello World</div>
       <div
-        className="flex rounded-xl bg-gradient-to-br from-zinc-50 to-zinc-300"
+        className="text-primary flex flex-col items-center justify-center rounded-xl bg-[#fefef8] px-6 py-3 font-mono"
         style={{
-          boxShadow: `4px 4px 12px 0px rgba(0, 0, 0, 0.1), -8px -8px 20px 0 rgba(255, 255, 255, 0.9),
-          inset 3px 3px 6px 0 rgba(0, 0, 0, 0.1), inset -6px -6px 12px 0 rgba(255, 255, 255, 1);`,
-          padding: '0px',
+          boxShadow: `4px 4px 6px 0px rgba(18, 61, 101, 0.2), -8px -8px 20px 0 rgba(255, 255, 255, 1);
+          `,
         }}
       >
-        <div className="text-primary h-full w-full rounded-xl bg-[#fffff8] px-6 py-3 font-mono">
+        <div>button</div>
+        <div
+          className="text-primary flex rounded-xl bg-[#fefef8] px-6 py-3 font-mono"
+          style={{
+            boxShadow: `4px 4px 6px 0px rgba(18, 61, 101, 0.2), -8px -8px 20px 0 rgba(255, 255, 255, 1);
+          `,
+          }}
+        >
           button
         </div>
       </div>
