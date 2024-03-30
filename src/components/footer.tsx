a5358b7ad3e61dbaaa5983fc8ddbbea3abc21ffe
Aydan Gooneratne <aydan@disczmusic.com>
2024-03-30 13:08:12 -0500

    actually fucking fire, needs some cleanup tho

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,14 @@
 import { FC } from '@/lib/utils/types'
 
 const Footer: FC = () => {
-  return <div className="relative z-20 h-footer w-full bg-background"></div>
+  return (
+    <div
+      style={{
+        background: 'linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, #000 60%, #000 100%)',
+      }}
+      className="relative z-20 h-footer w-full"
+    ></div>
+  )
 }
 
 export default Footer
